146532ef19fca041a7ab90352fd0b51284552e70
SeekerRook <xrist.vardakis@gmail.com>
2022-11-29 14:39:46 +0200

    removed unnecessary line of code

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ cp init.sh bin/cninit ;
 cp watchdog.sh bin/cnwatch ;
 cp notifier.sh bin/cnotifier ;
 cp flicker.py bin/cnflicker
-ls ~/.local/bin/caprine_notifications || 
-	mkdir ~/.local/bin/caprine_notifications &&
+#ls ~/.local/bin/caprine_notifications || 
+#	mkdir ~/.local/bin/caprine_notifications &&
 
 cp bin/* ~/.local/bin/ 
 
